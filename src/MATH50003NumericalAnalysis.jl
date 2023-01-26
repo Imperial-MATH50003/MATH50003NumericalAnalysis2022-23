f035ae797c9a28cc5666894f5615bc54675a3dd3
Sheehan Olver <solver@mac.com>
2023-01-26 16:00:48 +0000

    Lab 1 and Sheet 1 solutions (#3)
    * Lab 1 solutions
    
    * Problem Sheets 1
    
    * Sheet1 solution

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -27,6 +27,7 @@ notebook("src/notes/DualNumbers.jmd"; nkwds...)
 #####
 
 notebook("src/sheets/sheet1.jmd"; pkwds...)
+notebook("src/sheets/sheet1s.jmd"; pkwds...)
 
 #####
 # labs
@@ -34,8 +35,8 @@ notebook("src/sheets/sheet1.jmd"; pkwds...)
 
 import Literate
 
+Literate.notebook("src/labs/lab1s.jl", "labs/")
 Literate.notebook("src/labs/lab1.jl", "labs/")
-Literate.notebook("src/labs/lab2.jl", "labs/")
 
 Literate.notebook("src/labs/lab2s.jl", "labs/")
 write("src/labs/lab2.jl", replace(replace(read("src/labs/lab2s.jl", String), r"## SOLUTION(.*?)## END"s => ""), r"@test" => "@test_broken"))
