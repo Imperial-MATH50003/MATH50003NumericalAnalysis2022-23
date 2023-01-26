7523cf3642b85cdb918e7588390e28aad92d9b25
Sheehan Olver <solver@mac.com>
2023-01-26 10:48:36 +0000

    Lab 1 solutions

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -34,8 +34,8 @@ notebook("src/sheets/sheet1.jmd"; pkwds...)
 
 import Literate
 
+Literate.notebook("src/labs/lab1s.jl", "labs/")
 Literate.notebook("src/labs/lab1.jl", "labs/")
-Literate.notebook("src/labs/lab2.jl", "labs/")
 
 Literate.notebook("src/labs/lab2s.jl", "labs/")
 write("src/labs/lab2.jl", replace(replace(read("src/labs/lab2s.jl", String), r"## SOLUTION(.*?)## END"s => ""), r"@test" => "@test_broken"))
